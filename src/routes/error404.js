8c455f4b05220843f8b9df09607d5c559ec733c8
李健华 <lihua123569@163.com>
2017-09-26 18:31:11 +0800

    Update error404.js

--- src/routes/error404.js
+++ src/routes/error404.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { connect } from 'dva'; 
 import styles from './error404.css';
- 
+import { Breadcrumb,Icon } from 'antd';
+
 const  error404 = ({ location }) => {
   return (
-    
        <div className={styles.error404}>
-       404
+	       	<div className={styles.errorBreadcrumb}>
+		       	<Breadcrumb  separator=">">
+				    <Breadcrumb.Item><Icon type="home" /></Breadcrumb.Item>
+				    <Breadcrumb.Item><a href="">用户管理</a></Breadcrumb.Item>
+				    <Breadcrumb.Item>404</Breadcrumb.Item>
+			  	</Breadcrumb>
+			</div>
+       		<div>
+       			404
+       		</div>
         </div>
-    
   );
 }
 
